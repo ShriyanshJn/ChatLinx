9716eb9103f364af55e3c7a38b87baa8ba4fbc05
Shriyansh <shriyanshjain4444@gmail.com>
2023-07-19 00:08:24 +0530

    added push notifications

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setUpPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    // request permission from user to receive push notifications
+    await fcm.requestPermission();
+    //  address of the device on which our app is running, which we need to target this specific device (Here, sending push notifications using Firebase cloud messaging)
+    // final token = fcm.getToken();
+    // Since, we are using only one chat as a whole for all users we don't need to target a specific device as shown above,
+    // Instead we can send a push notification to all logged in users
+    fcm.subscribeToTopic('chat'); // after subscribing to this topic the device will receive push notifications from this topic 'chat
+  }
+
+  @override
+  void initState() {
+    setUpPushNotifications();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
